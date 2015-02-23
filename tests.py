8abacee7e3cf7e207913205f3150f0eda7403b84
David Ketcheson <dketch@gmail.com>
2015-02-23 14:50:45 +0300

    Call coverage from command line.
    So that function definition lines show as tested.

--- tests.py
+++ tests.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
 
 import matplotlib
-import nose
+import doctest
+import nodepy
 matplotlib.use('agg')
+import unittest
 
-nose.main()
+for module_name in ('runge_kutta_method',
+                    'linear_multistep_method',
+                    'ivp',
+                    'low_storage_rk',
+                    'rooted_trees',
+                    'snp',
+                    'stability_function',
+                    'general_linear_method',
+                    'ode_solver',
+                    'semidisc',
+                    'strmanip',
+                    'utils',
+                    'convergence'):
+    module = nodepy.__getattribute__(module_name)
+    doctest.testmod(module)
+
+unittest.main(module='nodepy.unit_tests',exit=False)
