40ed240c99eb26c327bcc736d01f1977dcd4c2c4
David Ketcheson <dketch@gmail.com>
2016-11-29 09:07:39 +0300

    Update to version 0.7.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='nodepy',
-      version='0.6.1',
+      version='0.7',
       packages=['nodepy'],
       author=['David Ketcheson'],
       author_email=['dketch@gmail.com'],
       url='http://numerics.kaust.edu.sa/nodepy/',
       description='Numerical ODE solvers in Python',
       license='modified BSD',
-      install_requires=['numpy','sympy'],
+      install_requires=['numpy','sympy','matplotlib','networkx'],
       )
