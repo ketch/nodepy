d973bb08cb1c06bd0935305dc4aafe96e6382fb6
David Ketcheson <dketch@gmail.com>
2021-09-08 14:32:22 +0300

    Use README as description on Pypi.

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 import setuptools
 from distutils.core import setup
 
+# Use README as description on Pypi
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(name='nodepy',
       version='1.0.1',
       packages=['nodepy'],
@@ -10,4 +15,6 @@ setup(name='nodepy',
       description='Numerical ODE solvers in Python',
       license='modified BSD',
       install_requires=['numpy','sympy','matplotlib'],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
       )
